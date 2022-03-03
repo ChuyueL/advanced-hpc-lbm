09303777c30a00da9cfc30ae6cc7510ef6a889ef
ChuyueL <chuyueluo@gmail.com>
2022-03-03 17:00:41 +0000

    changed env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,9 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
+export OMP_NUM_THREADS=28
+export OMP_PROC_BIND=true
+export OMP_PLACES=cores
+
+module load languages/anaconda2
+module load languages/intel
+module load languages/gcc
